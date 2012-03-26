3086ab68314044eead793d73416fbbc6268a354e
john <john@hesse>
2012-03-26 15:23:25 +0200

    display current activity

--- timetracker/tt.py
+++ timetracker/tt.py
@@ -3,6 +3,8 @@ CLI timetracker
 
 Sample usage:
 
+tt curr
+
 tt rec "reboot-1234"
 tt rec "reboot-666"
 
@@ -17,6 +19,8 @@ import os.path
 from argparse import ArgumentParser
 from datetime import datetime
 
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
 DATA_DIR = os.path.expanduser ('~/.timetracker/')
 
 if not os.path.exists (DATA_DIR):
@@ -25,13 +29,25 @@ if not os.path.exists (DATA_DIR):
 
 def _gen_record (t):
     d = datetime.now ()
-    return (str(d), t)
+    return (d.strftime (DATE_FORMAT), t)
+
+def _f ():
+    return os.path.join (DATA_DIR, 'log')
 
 def _w (r):
-    l = os.path.join (DATA_DIR, 'log')
-    with (open (l, 'a')) as f:
+    with (open (_f(), 'a')) as f:
         f.write (','.join (r))
         f.write ("\n")
+
+def current (p):
+    r = None
+    with (open (_f(), 'r')) as f:
+        r = list (f)[-1]
+
+    x = r.strip().split (',')
+    t = x [0]
+    d = datetime.now() - datetime.strptime (t, DATE_FORMAT)
+    print 'Current: %s, since %s (%s)' % (x[1], x[0], d)
 
 def new_record (p):
     r = _gen_record (p)
@@ -47,7 +63,8 @@ def summarize_period (p):
     print p
 
 CMDS = {
-    'rec' : new_record, 
+    'curr': current,
+    'rec': new_record, 
     'stop': stop, 
     'list': list_period, 
     'sum': summarize_period
