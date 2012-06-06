048d5ebd098b94babbc1ce18f5f79b0a3fc9c481
john <john@zlws-jj2.(none)>
2012-06-06 17:50:09 +0200

    fix install on linux systems

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
+cp vimrc/vimrc ~/.vimrc
+
 if [[ ! -d ~/.vim ]] ; then
-    echo 'Creating ~/.vim dir'
     mkdir ~/.vim ;
 fi
 
-echo 'Installing dependencies'
-
-# install dependencies.
-mkdir -p ~/.vim/autoload ~/.vim/bundle;
-curl -so ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-
-pushd .
-cd ~/.vim/bundle
-if [[ ! -d syntastic ]] ; then
-    git clone https://github.com/scrooloose/syntastic.git
-else
-    pwd
-    cd syntastic
-    git pull
-fi
-popd
-
-echo 'Installing vimrc'
-
-cp vimrc/vimrc ~/.vimrc
 cp -R vimrc/vim/* ~/.vim/
 
 /usr/bin/which -s ctags
