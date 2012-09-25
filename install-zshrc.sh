57e430bc96b8a315b90cbe394b939d270b12f4bf
john <john@zlws3>
2012-09-25 09:25:43 +0200

    install oh-my-zsh automatically

--- install-zshrc.sh
+++ install-zshrc.sh
@@ -4,8 +4,18 @@ cd ~/Desktop/dev-tools
 if [[ -d zsh-syntax-highlighting ]] ; then
     cd zsh-syntax-highlighting ;
     git pull ;
+    cd - ;
 else
     git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 fi
+pwd
+if [[ -d oh-my-zsh ]] ; then
+    cd oh-my-zsh ;
+    git pull ;
+    cd - ;
+else
+    git clone git://github.com/robbyrussell/oh-my-zsh.git
+fi
+
 echo "installed. to complete run:"
 echo "$ source ~/.zshrc"
