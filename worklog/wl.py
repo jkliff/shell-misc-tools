5635d31c94f3212f2b40907475c6cc7ee2e6d9c3
john <john@hesse>
2014-09-18 14:18:43 +0200

    worklog: edit file, init configuration

--- worklog/wl.py
+++ worklog/wl.py
@@ -1,70 +1,103 @@
 #!env python
 
+import datetime
 import os
-import sys
 from plumbum import local, FG
+import sys
+import yaml
 
 perr = lambda x: sys.stderr.write("%s\n" % x)
 
-def __publish (args):
+CONF_PATH = os.path.join(os.environ['H7R_WORKLOG_PATH'], 'wl.conf')
+
+_f = lambda mode: file(CONF_PATH, mode)
+empty_to_none = lambda x: None if x.strip() == '' else x
+etn = empty_to_none
+
+
+def load_conf():
+    return yaml.load(_f('r'))
+
+def save_conf(ctx):
+    yaml.dump(ctx, _f('w'))
+
+def __init(args):
+    ctx = {
+        'current_file': '',
+    }
+
+    if os.path.exists(CONF_PATH):
+        perr('Config file already exists. Aborting.')
+        return 2
+
+    conf = _f('w')
+    yaml.dump(ctx, conf)
+    print 'Created empty config file.'
+
+    return 0
+
+def __publish(args):
     pass
 
-def __save (args):
+def __save(args):
     pass
 
-def __edit (args):
+def __edit(args):
     if 'EDITOR' not in os.environ:
-        perr ('$EDITOR undefined')
+        perr('$EDITOR undefined')
         return 3
 
-    if 'H7R_WORKLOG_PATH' not in os.environ:
-        perr ('$H7R_WORKLOG_PATH undefined')
-        return 3
+    ctx = load_conf()
 
+    if '-n' in args or etn(ctx['current_file']) is None:
+        ctx['current_file'] = 'wl_%s.md' % datetime.date.today().isoformat()
 
-    log_path = os.path.join (os.environ['H7R_WORKLOG_PATH'], 'foo')
-    if not os.path.exists (log_path):
-        local['touch'][log_path]()
+    log_path = os.path.join(os.environ['H7R_WORKLOG_PATH'], ctx['current_file'])
+    if not os.path.exists(log_path):
+        print 'Creating new worklog file %s' % ctx['current_file']
+        with(open(log_path, 'w')) as f:
+            f.write('<!--- H7R worklog file %s; on %s --->' %(ctx['current_file'], datetime.date.today().isoformat()))
 
     editor = local[os.environ['EDITOR']]
     editor[log_path] & FG
 
+    save_conf(ctx)
+
     return 0
 
 CMDS = {
+    'init': __init,
     'pub': __publish,
     'save': __save,
     'edit': __edit
 }
 
-def parse_args (args):
-
-    #from argparse import ArgumentParser
-    #parser = ArgumentParser (prog=args[0])
+def parse_args(args):
 
     v = args [1:]
     cmd = 'edit'
 
-    if len (v) > 0 and not v[0].startswith ('--'):
-        if len (v) > 1 and v[1] == '--':
+    if len(v) > 0 and not v[0].startswith('--'):
+        if len(v) > 1 and v[1] == '--':
             v = [v[0]] + v[2:]
         cmd = v[0]
         v = v[1:]
 
-    #parser.add_argument('command', default='edit', nargs='?')
-
-    #return cmd, parser.parse_args (v)
     return cmd, v
 
-def main (argv):
+def main(argv):
 
-    cmd, args = parse_args (argv)
-    print cmd, args
+    if 'H7R_WORKLOG_PATH' not in os.environ:
+        perr('$H7R_WORKLOG_PATH undefined')
+        return 3
+
+    cmd, args = parse_args(argv)
+
     if cmd not in CMDS.keys():
-        perr ('Uknown command: %s' % cmd)
+        perr('Uknown command: %s' % cmd)
         return 2
 
     return CMDS[cmd](args)
 
 if __name__ == '__main__':
-    sys.exit (main(sys.argv))
+    sys.exit(main(sys.argv))
