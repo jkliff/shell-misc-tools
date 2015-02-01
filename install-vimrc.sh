942ff51f196e91dd8d180220c444723a9633d135
john <john@hesse>
2015-02-01 14:10:59 +0100

    add help, switches for backup and debug, minor adjustments

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -1,15 +1,29 @@
 #!env bash
 
-cp -v vimrc/vimrc ~/.vimrc
+set -e
+[[ "$@" == "--debug" ]] && set -x
 
-if [[ ! -d ~/.vim ]] ; then
-    mkdir ~/.vim ;
-fi
+function usage {
+    echo "Installer for vimrc and bundles"
+    echo "Overwrite \$BUNDLE_DIR to set director to other than ~/.vim/bundle"
+    echo "Options:"
+    echo "    --debug"
+    echo "    --backup"
+}
+
+
+[[ "$@" == "--help" ]] && usage && exit
+
+[[ -z ${BUNDLE_DIR+x} ]] && BUNDLE_DIR=~/.vim/bundle
+echo Using bundle dir [$BUNDLE_DIR]
+
+cp -v vimrc/vimrc ~/.vimrc
+[[ ! -d ~/.vim ]] && mkdir ~/.vim
 
 echo 'Installing dependencies'
 
 # install dependencies.
-mkdir -p ~/.vim/autoload ~/.vim/bundle;
+mkdir -p ~/.vim/autoload $BUNDLE_DIR;
 
 CURL=`which curl`
 if [[ $CURL != "" ]] ; then
@@ -22,7 +36,7 @@ fi
 $CURL ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 pushd .
-cd ~/.vim/bundle
+cd $BUNDLE_DIR
 
 BUNDLES="https://github.com/scrooloose/syntastic.git \
     https://github.com/nvie/vim-flake8.git \
@@ -65,9 +79,9 @@ fi
 
 popd
 
-echo 'Installing vimrc plugins (not bundles)'
-mv -v ~/.vim/plugin ~/.vim/plugin.old-`date +%F-%H%M%S`
-cp -Rv vimrc/vim/* ~/.vim/
+echo 'Installing/updating vimrc plugins (not bundles)'
+[[ "$@" == "--backup" ]] && echo "Backing old plugins up" && mv -v ~/.vim/plugin ~/.vim/plugin.old-`date +%F-%H%M%S`
+cp -Rvu vimrc/vim/* ~/.vim/
 
 /usr/bin/which ctags > /dev/null
 [[ $? != "0" ]] && echo "ctags not found. Install for your platform for full features."
